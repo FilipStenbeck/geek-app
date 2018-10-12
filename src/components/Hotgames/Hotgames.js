74c2ab9ccdb574a735ed7272066ab78e6ab269e2
Filip Stenbeck <filip.stenbeck@nordnet.se>
2018-10-12 11:33:11 +0200

    mobile first

--- src/components/Hotgames/Hotgames.js
+++ src/components/Hotgames/Hotgames.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import compose from 'recompose/compose';
 import { withStyles } from '@material-ui/core/styles';
 import GridList from '@material-ui/core/GridList';
 import GridListTile from '@material-ui/core/GridListTile';
@@ -7,7 +8,7 @@ import GridListTileBar from '@material-ui/core/GridListTileBar';
 import ListSubheader from '@material-ui/core/ListSubheader';
 import IconButton from '@material-ui/core/IconButton';
 import InfoIcon from '@material-ui/icons/Info';
-//import tileData from './tileData';
+import withWidth, { isWidthUp } from '@material-ui/core/withWidth';
 
 const styles = theme => ({
   root: {
@@ -18,7 +19,11 @@ const styles = theme => ({
     backgroundColor: theme.palette.background.paper,
   },
   gridList: {
-    width: '60%',
+    width: '80%',
+  },
+  tile: {
+    marginBottom: 6,
+    maxWidth: 300,
   },
   icon: {
     color: 'rgba(255, 255, 255, 0.54)',
@@ -26,21 +31,51 @@ const styles = theme => ({
 });
 
 function TitlebarGridList(props) {
-  const { classes, games } = props;
+  const { classes, games, width } = props;
 
   const tileData = games.map(game => ({
     img: game.thumbnail,
     title: game.name,
   }));
 
+  //TODO This need refactoring
+  if (isWidthUp('sm', width)) {
+    return (
+      <div className={classes.root}>
+        <GridList className={classes.gridList} cellHeight={280}>
+          <GridListTile key="Subheader" cols={2} style={{ height: 'auto' }}>
+            <ListSubheader component="div">
+              <h3>Popular games</h3>
+            </ListSubheader>
+          </GridListTile>
+          {tileData.map(tile => (
+            <GridListTile className={classes.tile} key={tile.img}>
+              <img src={tile.img} alt={tile.title} />
+              <GridListTileBar
+                title={tile.title}
+                actionIcon={
+                  <IconButton className={classes.icon}>
+                    <InfoIcon />
+                  </IconButton>
+                }
+              />
+            </GridListTile>
+          ))}
+        </GridList>
+      </div>
+    );
+  }
+
   return (
     <div className={classes.root}>
-      <GridList cellHeight={280} className={classes.gridList}>
-        <GridListTile key="Subheader" cols={2} style={{ height: 'auto' }}>
-          <ListSubheader component="div">Hot games</ListSubheader>
+      <GridList cols={1} cellHeight={280} className={classes.gridList}>
+        <GridListTile key="Subheader" cols={1} style={{ height: 'auto' }}>
+          <ListSubheader component="div">
+            <h3>Popular games</h3>
+          </ListSubheader>
         </GridListTile>
         {tileData.map(tile => (
-          <GridListTile key={tile.img}>
+          <GridListTile className={classes.tile} cols={1} key={tile.img}>
             <img src={tile.img} alt={tile.title} />
             <GridListTileBar
               title={tile.title}
@@ -61,4 +96,7 @@ TitlebarGridList.propTypes = {
   classes: PropTypes.object.isRequired,
 };
 
-export default withStyles(styles)(TitlebarGridList);
+export default compose(
+  withStyles(styles),
+  withWidth()
+)(TitlebarGridList);
